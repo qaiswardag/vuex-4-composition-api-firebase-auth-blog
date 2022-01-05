73beecfcc375c29d18c0313c7c05bd169794ec27
qaiswardag <qais.wardag@outlook.com>
2022-01-05 09:37:51 +0100

    Add firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,64 @@
+// import { initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyB4UNtLm1gG-_afnSj5BpIPxK41F4cKwJQ',
+//   authDomain: 'teach-firebase-and-firestore.firebaseapp.com',
+//   projectId: 'teach-firebase-and-firestore',
+//   storageBucket: 'teach-firebase-and-firestore.appspot.com',
+//   messagingSenderId: '326075475560',
+//   appId: '1:326075475560:web:8b0eb2094b73418b5d7ddd',
+// };
+
+// // 1.
+// // init firebase app using firebase initializeApp() function from firebase library
+// // remember to import the initializeApp function so we can use it
+// //
+// initializeApp(firebaseConfig);
+
+// // init the individual firebase products or services: In this project we will only need auth
+
+// // 1. auth services
+// // init firebase auth.
+// // init firebase auth will return something to us. so: getAuth will return something
+// // we can fx use it in our components or vuex store to interact to ex sign user in, out or signup
+// const auth = getAuth();
+
+// // now we need to export the  auth property so we can use it in another file
+// export { auth };
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// try to use init our firebase webapp and init firebase auth service below
+// if need help look in the top of the file
